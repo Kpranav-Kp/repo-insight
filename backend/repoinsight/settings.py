5261a5f7ec5ec77326dd5622eb41307ef0d8e94f
Kpranav-Kp <pranav2005.kp@gmail.com>
2026-03-11 14:40:14 +0530

    POSTGRESQL setup

--- backend/repoinsight/settings.py
+++ backend/repoinsight/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "repoinsight.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'repoinsight_db',
+        'USER': 'repoinsight_user',
+        'PASSWORD': 'StrongPassword123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
