d17dc2736ecd9f68e16f2a251a439c00b6862c6f
Kpranav-Kp <pranav2005.kp@gmail.com>
2026-04-04 23:21:01 +0530

    Urls endpoints

---
+++ backend/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import RepositoryStatusView, RepositoryAnalyzeView, ChatSessionView, ChatMessageView, RecommendationFeedbackView
+
+urlpatterns = [
+    path('repositories/<int:repo_id>/status/', RepositoryStatusView.as_view(), name='repository-status'),
+    path('repositories/analyze/', RepositoryAnalyzeView.as_view(), name='repository-analyze'),
+    path('chat/session/', ChatSessionView.as_view(), name='chat-session'),
+    path('chat/message/', ChatMessageView.as_view(), name='chat-message'),
+    path('recommendations/<int:reccomendation_id>/feedback/', RecommendationFeedbackView.as_view())
+]
